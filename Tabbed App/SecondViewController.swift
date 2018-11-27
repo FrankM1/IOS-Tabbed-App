d1d68dee59c1e11e9ce99db4fe3f6b9d3395d33f
frank.gitonga <frank.gitonga@incentrocom>
2018-11-27 13:52:02 +0300

    Initial commit

---
+++ Tabbed App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed App
+//
+//  Created by Frank on 11/12/18.
+//  Copyright © 2018 Qazana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
